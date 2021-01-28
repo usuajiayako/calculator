c33beab1de783d02a5b09d7bc4d26523d7535be1
JJYoko101 <mould_joe@hotmail.com>
2021-01-28 16:16:34 +0000

    built basic calculator functionality

--- CHECKLIST.md
+++ CHECKLIST.md
@@ -1,4 +1,5 @@
 ## Planning
+
 [check] Copy this list into your project
 [check] Outline functionality of your app
 [check] Draw what the user will see
@@ -10,6 +11,7 @@
 [ ] Determine from this where you will require state & hold functions
 
 ## Setup
+
 [check] Create your app directory (e.g. with create-react-app)
 [check] Run your app
 [check] Ensure you have React DevTools running (in browser)
@@ -18,8 +20,19 @@
 [ ] Check everything is hooked up
 
 ## Further functionality
+
 [ ] Flesh out your components with methods - this is a pretty big step!
 
 ## Style
+
 [ ] Add classNames
 [ ] Create and import .css file(s)
+
+## Function Items
+
+[ check ] Equals sign click to trigger a function
+[ check ] Function to evaluate as result of click and calculate
+[ ] Style the calculator using css
+[ ]
+[ ]
+[ ]
--- src/App.js
+++ src/App.js
@@ -1,68 +1,135 @@
-import "./App.css";
-import React from "react";
+import './App.css';
+import React from 'react';
 
 class App extends React.Component {
   state = {
-    displayText : "",
+    displayText: '',
     lookUp: {
-      btn1: "1",
-      btn2: "2",
-      btn3: "3",
-      btn4: "4",
-      btn5: "5",
-      btn6: "6",
-      btn7: "7",
-      btn8: "8",
-      btn9: "9",
-      btn0: "0",
-      btnmin: "-",
-      btnplus: "+",
-      btnmulti: "*",
-      btndiv: "/",
-      btnLeft: "(",
-      btnRight: ")",
+      btn1: '1',
+      btn2: '2',
+      btn3: '3',
+      btn4: '4',
+      btn5: '5',
+      btn6: '6',
+      btn7: '7',
+      btn8: '8',
+      btn9: '9',
+      btn0: '0',
+      btnmin: '-',
+      btnplus: '+',
+      btnmulti: '*',
+      btndiv: '/',
+      btnLeft: '(',
+      btnRight: ')'
     }
-  }
+  };
   render() {
     return (
-      <div>
+      <div class="calculator">
         <h1>Calculator</h1>
-      
-        <input type="text" onChange={this.handleInput} value={this.state.displayText}></input>
+
+        <input
+          type="text"
+          onChange={this.handleInput}
+          value={this.state.displayText}
+          class="calc-row"
+        ></input>
         {/* 7, 8, 9, + */}
-        <button onClick={this.handleClick} id="btn7">7</button>
-        <button onClick={this.handleClick} id="btn8">8</button>
-        <button onClick={this.handleClick} id="btn9">9</button>
-        <button onClick={this.handleClick} id="btnplus">+</button>
+        <div class="calc-row">
+          <button onClick={this.handleClick} id="btn7">
+            7
+          </button>
+          <button onClick={this.handleClick} id="btn8">
+            8
+          </button>
+          <button onClick={this.handleClick} id="btn9">
+            9
+          </button>
+          <button onClick={this.handleClick} id="btnplus">
+            +
+          </button>
+        </div>
         {/* 4, 5, 6, - */}
-        <button onClick={this.handleClick} id="btn4">4</button>
-        <button onClick={this.handleClick} id="btn5">5</button>
-        <button onClick={this.handleClick} id="btn6">6</button>
-        <button onClick={this.handleClick} id="btnmin">-</button>
+        <div class="calc-row">
+          <button onClick={this.handleClick} id="btn4">
+            4
+          </button>
+          <button onClick={this.handleClick} id="btn5">
+            5
+          </button>
+          <button onClick={this.handleClick} id="btn6">
+            6
+          </button>
+          <button onClick={this.handleClick} id="btnmin">
+            -
+          </button>
+        </div>
         {/* 1, 2, 3, * */}
-        <button onClick={this.handleClick} id="btn1">1</button>
-        <button onClick={this.handleClick} id="btn2">2</button>
-        <button onClick={this.handleClick} id="btn3">3</button>
-        <button onClick={this.handleClick} id="btnmulti">*</button>
-        {/* 0, =, */}
-        <button onClick={this.handleClick} id="btn0">0</button>
-        <button onClick="dummy">=</button>
-        <button onClick={this.handleClick} id="btnLeft">(</button>
-        <button onClick={this.handleClick} id="btnRight">)</button>
+        <div class="calc-row">
+          <button onClick={this.handleClick} id="btn1">
+            1
+          </button>
+          <button onClick={this.handleClick} id="btn2">
+            2
+          </button>
+          <button onClick={this.handleClick} id="btn3">
+            3
+          </button>
+          <button onClick={this.handleClick} id="btnmulti">
+            *
+          </button>
+        </div>
+        {/* 0, /, */}
+        <div class="calc-row">
+          <button onClick={this.handleClick} id="btn0">
+            0
+          </button>
+          <button onClick={this.handleClick} id="btnLeft">
+            (
+          </button>
+          <button onClick={this.handleClick} id="btnRight">
+            )
+          </button>
+          <button onClick={this.handleClick} id="btndiv">
+            /
+          </button>
+        </div>
+        {/* = */}
+        <button onClick={this.handleSum} class="calc-row" id="btneqls">
+          =
+        </button>
       </div>
-    )
+    );
   }
-  handleClick = ({target: {id}}) => {
-    this.setState((currentState) => {
-      return { displayText : currentState.displayText + currentState.lookUp[id]}
-    })
-  }
+  handleClick = ({ target: { id } }) => {
+    this.setState(currentState => {
+      return {
+        displayText: currentState.displayText + currentState.lookUp[id]
+      };
+    });
+  };
 
-  displayText = () => {
-    
-  }
+  handleSum = () => {
+    this.setState(currentState => {
+      const numRegex = /(\d+)[\D](\d+)/;
+      const symbolRegex = /\D/;
+
+      const x = +currentState.displayText.match(numRegex)[1];
+      const y = +currentState.displayText.match(numRegex)[2];
+      const symbol = currentState.displayText.match(symbolRegex)[0];
+
+      const calculation = this.binaryCalculate(x, y, symbol);
+
+      return { displayText: calculation };
+    });
+  };
+
+  binaryCalculate = (x, y, symbol) => {
+    if (symbol === '+') return x + y;
+    if (symbol === '-') return x - y;
+    if (symbol === '*') return x * y;
+    if (symbol === '/') return x / y;
+  };
 }
-
-
 
 export default App;
