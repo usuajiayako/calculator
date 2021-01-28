7c90c4bb58095312114ea15e7bcb0b24967566d1
Ayako <usuajiayako@gmail.com>
2021-01-28 15:19:50 +0000

    connect bottons

--- CHECKLIST.md
+++ CHECKLIST.md
@@ -1,8 +1,8 @@
 ## Planning
-[ ] Copy this list into your project
-[ ] Outline functionality of your app
-[ ] Draw what the user will see
-[ ] Break out components from your sketch
+[check] Copy this list into your project
+[check] Outline functionality of your app
+[check] Draw what the user will see
+[check] Break out components from your sketch
 [ ] Name your components
 [ ] Draw component hierarchy
 [ ] Label where you will require functions that change state
@@ -10,10 +10,10 @@
 [ ] Determine from this where you will require state & hold functions
 
 ## Setup
-[ ] Create your app directory (e.g. with create-react-app)
-[ ] Run your app
-[ ] Ensure you have React DevTools running (in browser)
-[ ] Make blank components (functional or class?) as per your plan
+[check] Create your app directory (e.g. with create-react-app)
+[check] Run your app
+[check] Ensure you have React DevTools running (in browser)
+[check] Make blank components (functional or class?) as per your plan
 [ ] Export and import components as per your hierarchy
 [ ] Check everything is hooked up
 
--- src/App.js
+++ src/App.js
@@ -2,34 +2,60 @@ import "./App.css";
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    displayText : "",
+    lookUp: {
+      btn1: "1",
+      btn2: "2",
+      btn3: "3",
+      btn4: "4",
+      btn5: "5",
+      btn6: "6",
+      btn7: "7",
+      btn8: "8",
+      btn9: "9",
+      btn0: "0",
+      btnmin: "-",
+      btnplus: "+",
+      btnmulti: "*",
+      btndiv: "/",
+    }
+  }
   render() {
-    console.log("hello")
     return (
       <div>
-        <h1>Title</h1>
-        <input type="text"></input>
+        <h1>Calculator</h1>
+      
+        <input type="text" onChange={this.handleInput} value={this.state.displayText}></input>
         {/* 7, 8, 9, + */}
-        <button onClick="dummy">7</button>
-        <button onClick="dummy">8</button>
-        <button onClick="dummy">9</button>
-        <button onClick="dummy">+</button>
+        <button onClick={this.handleClick} id="btn7">7</button>
+        <button onClick={this.handleClick} id="btn8">8</button>
+        <button onClick={this.handleClick} id="btn9">9</button>
+        <button onClick={this.handleClick} id="btnplus">+</button>
         {/* 4, 5, 6, - */}
-        <button onClick="dummy">4</button>
-        <button onClick="dummy">5</button>
-        <button onClick="dummy">6</button>
-        <button onClick="dummy">-</button>
+        <button onClick={this.handleClick} id="btn4">4</button>
+        <button onClick={this.handleClick} id="btn5">5</button>
+        <button onClick={this.handleClick} id="btn6">6</button>
+        <button onClick={this.handleClick} id="btnmin">-</button>
         {/* 1, 2, 3, * */}
-        <button onClick="dummy">1</button>
-        <button onClick="dummy">2</button>
-        <button onClick="dummy">3</button>
-        <button onClick="dummy">*</button>
+        <button onClick={this.handleClick} id="btn1">1</button>
+        <button onClick={this.handleClick} id="btn2">2</button>
+        <button onClick={this.handleClick} id="btn3">3</button>
+        <button onClick={this.handleClick} id="btnmulti">*</button>
         {/* 0, =, */}
-        <button onClick="dummy">0</button>
+        <button onClick={this.handleClick} id="btn0">0</button>
         <button onClick="dummy">=</button>
-
-        
       </div>
     )
+  }
+  handleClick = ({target: {id}}) => {
+    this.setState((currentState) => {
+      return { displayText : currentState.displayText + currentState.lookUp[id]}
+    })
+  }
+
+  displayText = () => {
+    
   }
 }
 
